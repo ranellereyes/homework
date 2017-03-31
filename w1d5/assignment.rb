3b7293fb003dc8e32298a2705c09cff6f63a2508
ranellereyes <ranelle.reyes@gmail.com>
2017-03-31 00:55:40 -0700

    Created class Stack and initialized with empty array

---
+++ w1d5/assignment.rb
@@ -0,0 +1,19 @@
+class Stack
+  attr_accessor :stack
+
+  def initialize
+    @stack = []
+  end
+
+  def add(el)
+    # adds an element to the stack
+  end
+
+  def remove
+    # removes one element from the stack
+  end
+
+  def show
+    # return a copy of the stack
+  end
+end
